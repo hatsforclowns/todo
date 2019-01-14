b24936c2d06ea1cac2bb4f9910ab3fe51f756142
Alex Ford <helloalexford@gmail.com>
2019-01-14 11:30:58 +0000

    Feature: Sort tasks by name/colour

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,51 @@
   <section id="app" class="section">
     <div class="container">
       <h1>Places to eat</h1>
-      <div class="field has-addons">
-        <div class="control is-expanded">
-          <input
-            v-model="inputText"
-            ref="taskInput"
-            class="input is-medium"
-            type="text"
-            placeholder="Add a task..."
-            @keyup.enter="addTask(inputText)"
-          >
+      <div class="columns is-multiline">
+        <div class="column is-12-touch is-7-desktop">
+          <div class="field has-addons">
+            <div class="control is-expanded">
+              <input
+                v-model="inputText"
+                ref="taskInput"
+                class="input is-medium"
+                type="text"
+                placeholder="Add a task..."
+                @keyup.enter="addTask(inputText)"
+              >
+            </div>
+            <div class="control">
+              <button
+                type="submit"
+                class="button is-medium is-primary has-text-weight-semibold"
+                @click="addTask(inputText)"
+              >
+                <span class="icon is-medium">
+                  <i class="mdi mdi-24px mdi-plus"></i>
+                </span>
+                <span>Add task</span>
+              </button>
+            </div>
+          </div>
         </div>
-        <div class="control">
-          <button
-            type="submit"
-            class="button is-medium is-primary has-text-weight-semibold"
-            @click="addTask(inputText)"
-          >
-            <span class="icon is-medium">
-              <i class="mdi mdi-24px mdi-plus"></i>
-            </span>
-            <span>Add task</span>
-          </button>
+        <div class="column is-hidden-touch"/>
+        <div class="column is-12-touch is-3-desktop">
+          <div class="field">
+            <div class="control is-expanded">
+              <div class="select is-fullwidth">
+                <select v-model="sortSchema">
+                  <option value="undefined">Sort: most recent</option>
+                  <option value="name">Sort: name</option>
+                  <option value="colour">Sort: colour</option>
+                </select>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Task
         @delete="removeTask(task)"
-        v-for="task in tasks"
+        v-for="task in sortedTasks"
         :key="task.name"
         :name="task.name"
         :colour="task.colour"
@@ -49,8 +67,25 @@ export default {
     return {
       inputText: undefined,
       colourIndex: undefined,
+      sortSchema: undefined,
       tasks: new Array()
     };
+  },
+  computed: {
+    sortedTasks() {
+      // Proxy 'this' to avoid side-effects
+      const vm = this;
+
+      // If sort schema set, pass tasks array to sortTasks method
+      if (vm.sortSchema == "name") {
+        return vm.sortTasks("name");
+      } else if (vm.sortSchema == "colour") {
+        return vm.sortTasks("colour");
+      }
+
+      // If no sort schema, return original tasks array
+      return vm.tasks;
+    }
   },
   mounted() {
     // Populate app with sample tasks
@@ -97,6 +132,18 @@ export default {
 
       // Remove selected task from tasks array
       return this.tasks.splice(task, 1);
+    },
+    sortTasks(prop) {
+      // Set sort schema
+      this.sortSchema = prop;
+
+      // Clone original tasks array
+      const tasks = this.tasks.slice(0);
+
+      // Return new array sorted by prop argument (name/colour)
+      return tasks.sort((a, b) =>
+        a[prop] > b[prop] ? 1 : b[prop] > a[prop] ? -1 : 0
+      );
     }
   }
 };
@@ -108,6 +155,7 @@ $mdi-font-path: "~@mdi/font/fonts";
 @import "~@mdi/font/scss/materialdesignicons";
 @import "~bulma/sass/utilities/all";
 @import "~bulma/sass/base/all";
+@import "~bulma/sass/grid/columns";
 @import "~bulma/sass/layout/section";
 @import "~bulma/sass/elements/container";
 @import "~bulma/sass/elements/form";
