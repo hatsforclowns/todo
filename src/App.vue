510547ba1822c952e9b5a65fcc72f0418f0b3617
Alex Ford <helloalexford@gmail.com>
2020-03-07 17:12:20 +0000

    Scaffold app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="app" class="section">
+    <div class="container">
+      <h1>Places to eat</h1>
+      <div class="field has-addons">
+        <div class="control is-expanded">
+          <input
+            v-model="inputText"
+            ref="taskInput"
+            class="input is-medium"
+            type="text"
+            placeholder="Add a task..."
+            @keyup.enter="addTask(inputText)"
+          >
+        </div>
+        <div class="control">
+          <button
+            type="submit"
+            class="button is-medium is-primary has-text-weight-semibold"
+            @click="addTask(inputText)"
+          >
+            <span class="icon is-medium">
+              <i class="mdi mdi-24px mdi-plus"></i>
+            </span>
+            <span>Add task</span>
+          </button>
+        </div>
+      </div>
+      <Task @delete="removeTask(task)" v-for="task in tasks" :key="task" :name="task"/>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Task from "@/components/ToDoTask.vue";
 export default {
-  name: 'app',
+  name: "todo",
   components: {
-    HelloWorld
+    Task
+  },
+
+  data() {
+    return {
+      inputText: undefined,
+      tasks: new Array()
+    };
+  },
+  mounted() {
+    // Populate app with sample tasks
+    this.addTask("Dirty Bones");
+    this.addTask("Blanchette");
+    this.addTask("Chez Elles Bistroquet");
+    this.addTask("Sticky Wings");
+    this.addTask("Nha-Mor");
+    this.addTask("Yuu Kitchen");
+    this.addTask("E Pellicci");
+    this.addTask("Sushi Samba");
+    this.addTask("Ping Pong");
+    this.addTask("Madame D");
+    this.addTask("Azou");
+    this.addTask("Alto");
+    this.addTask("Passo");
+    this.addTask("Arancini Factory");
+    this.addTask("Flat Iron");
+    this.addTask("Lure Fish Kitchen");
+    this.addTask("Lord Palmerston");
+  },
+  methods: {
+    addTask(name) {
+      // Add defined task to the end of the 'tasks' array
+      this.tasks.push(name);
+
+      // Clear the task input field
+      this.inputText = undefined;
+
+      // Set cursor focus to input field
+      this.$refs.taskInput.focus();
+    },
+    removeTask(name) {
+      // Get index of selected task
+      const task = this.tasks.indexOf(name);
+
+      // Remove selected task from tasks array
+      return this.tasks.splice(task, 1);
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+$mdi-font-path: "~@mdi/font/fonts";
+
+@import "~@mdi/font/scss/materialdesignicons";
+@import "~bulma/sass/utilities/all";
+@import "~bulma/sass/base/all";
+@import "~bulma/sass/layout/section";
+@import "~bulma/sass/elements/container";
+@import "~bulma/sass/elements/form";
+@import "~bulma/sass/elements/icon";
+@import "~bulma/sass/elements/button";
+
+h1 {
+  font-size: $size-1;
+  font-weight: $weight-bold;
+  margin-bottom: $size-1 * 0.66;
 }
 </style>
---
+++ src/components/ToDoTask.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="task">
+    <div class="task-name">{{ name }}</div>
+    <div class="task-delete" @click="$emit('delete', name)">
+      <span class="icon is-medium">
+        <i class="mdi mdi-24px mdi-close"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~bulma/sass/utilities/all";
+
+.task {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid $grey-lighter;
+  transition: background-color 0.1s ease-out;
+
+  &:hover {
+    background-color: transparentize($info, 0.97);
+  }
+
+  &-name {
+    padding: $size-6 0 $size-6 $size-6;
+    font-weight: $weight-semibold;
+  }
+
+  &-delete {
+    padding: $size-6;
+    opacity: .2;
+    transition: opacity 0.1s ease-out;
+
+    @include desktop() {
+      opacity: 0;
+    }
+
+    span {
+      transition: color 0.1s ease-out;
+    }
+
+    &:hover {
+      cursor: pointer;
+
+      span {
+        color: $danger;
+      }
+    }
+
+    @at-root .task:hover & {
+      opacity: 1;
+    }
+  }
+}
+</style>
